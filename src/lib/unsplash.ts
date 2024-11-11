10ca611836c5b1e12443d926703d08d3e496b144
Wahajyousafzai <161183135+Wahajyousafzai@users.noreply.github.com>
2024-11-11 12:22:58 +0500

    update

--- src/lib/unsplash.ts
+++ src/lib/unsplash.ts
@@ -1,18 +1,18 @@
-import { createApi } from 'unsplash-js';
-
-const unsplash = createApi({
-  accessKey: process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY!,
-});
-
-export const getFashionImages = async (query: string) => {
-  const result = await unsplash.search.getPhotos({
-    query: `mens ${query} fashion`,
-    perPage: 1,
-    orientation: 'landscape',
-    orderBy: 'random',
-  });
-
-  return result.response?.results[0];
-};
-
-export default unsplash; 
+import { createApi } from 'unsplash-js';
+
+const unsplash = createApi({
+  accessKey: process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY!,
+});
+
+export const getFashionImages = async (query: string) => {
+  const result = await unsplash.photos.getRandom({
+    query: `mens ${query} fashion`,
+    orientation: 'landscape',
+  });
+
+  return Array.isArray(result.response) 
+    ? result.response[0] 
+    : result.response;
+};
+
+export default unsplash; 
